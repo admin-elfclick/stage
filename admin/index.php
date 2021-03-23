d6ecf2434bb69c7de4fc2864c151107c7e93e531
Developer Himel <programmerhimel@gmail.com>
2021-03-23 23:05:37 +0600

    v.1.2

---
+++ admin/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Panel</title>
+    <link rel="stylesheet" href="./admin.css">
+</head>
+<body>
+    <div>
+        <center>
+            <h1>Admin Panel is under Development.</h1>
+        </center>
+        <?php require('dbconnection.php') ?>
+    </div>
+</body>
+</html>
